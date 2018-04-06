ecd646e5a871a466f0c38567b42f500f3ce11c47
Giulian Drimba <giuliandrimba@gmail.com>
2018-04-06 00:20:46 -0300

    type v4

--- src/letter.js
+++ src/letter.js
@@ -10,51 +10,25 @@ class Letter {
     this.y = y + 25;
     this.form = form;
     this.ctx = ctx;
-    this.MAX_FITNESS = undefined;
-    this.fitnessScore = undefined;
     this.done = false;
-    this.MAX = window.innerWidth / 2
-    this.numDone = 0;
     this.CENTER_X = x;
     this.CENTER_Y = y;
 
-    this.done = false;
-    this.dna = DNA.generate(this.form, ctx.canvas.width, ctx.canvas.height);
-    this.points = this.dna;
+    this.points = this.form;
     this.lines = [];
-    this.MAX_FITNESS = Calc.dist(0,0,70,140) * 5;
-    this.fitnessScore = 0
-  }
-
-  update() {
-    for(var i = 0; i < this.points.length; i++) {
-      var p = this.points[i];
-      TweenMax.to(p, 0.15, {x:this.dna[i].x, y:this.dna[i].y, override:true})
-    }
-  }
-
-  reset() {
-    this.dna = DNA.generate()
-    this.points = this.dna;
-    this.lines = [];
-    this.circles = [];
-    this.MAX_FITNESS = Calc.dist(0,0,70,140) * 5;
-    this.done = false;
   }
 
   draw() {
-    var total = this.dna.length;
-    this.ctx.fillStyle = 'white';
-    this.ctx.strokeStyle = 'rgba(0,0,0, 0.4)';
-    this.ctx.globalCompositeOperation = "hard-light";
+    var total = this.form.length;
+    this.ctx.fillStyle = 'black';
+    // this.ctx.strokeStyle = 'rgba(0,0,0, 1)';
+    // this.ctx.globalCompositeOperation = "hard-light";
     this.ctx.beginPath();
-    for (var i = 0; i < total; i++) {
-      let x = this.x + parseFloat(this.dna[i].x);
-      let y = this.y + parseFloat(this.dna[i].y);
-      let r = this.dna[i].r * 0.5;
+    for (var i = 0; i < total; i+=2) {
+      let x = this.x + parseFloat(this.form[i])
+      let y = this.y + parseFloat(this.form[i+1])
       this.drawline(this.CENTER_X, this.CENTER_Y, x, y)
-      // this.drawRect(x, y, r);
-      // ctx.stroke();
+      // this.drawRect(x, y, 10);
     }
     // this.ctx.fill();
     this.ctx.stroke();
@@ -69,36 +43,6 @@ class Letter {
     this.ctx.moveTo(fromX, fromY);
     this.ctx.lineTo(x,y);
   }
-
-  evolve() {
-    let dist = undefined;
-    let count = 0;
-    if (this.numDone > this.dna.length * 0.8) {
-      return false;
-    }
-    this.numDone = 0
-    
-    this.dna = this.dna.map((item, i)=>{
-      dist = Calc.dist(this.form[count], this.form[count + 1], item.x, item.y);
-      if (dist > 2) {
-        item.x = Math.min(this.MAX,parseFloat(item.x) + randomRange(dist))
-        item.y = Math.min(this.MAX,parseFloat(item.y) + randomRange(dist))
-        item.r = dist / 5
-      } else {
-        item.r = 50
-        this.numDone++;
-      }
-      count = i * 2;
-      return item;
-    })
-    return true;
-  }
-}
-
-function randomRange(amount) {
-  var num = Math.floor(Math.random() * amount) + 1; // this will get a number between 1 and 99;
-  num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1; // this will add minus sign in 50% of cases
-  return num;
 }
 
 module.exports = Letter;
--- src/sketch.js
+++ src/sketch.js
@@ -3,66 +3,39 @@ var Letter = require('./letter');
 
 var ctx = undefined;
 var env = undefined;
-var imageCanvas = undefined;
-var image = undefined;
-var texts = []
-var NUM_COLS = 20;
-var COL_WIDTH = 0;
-var NUM_ROWS = 20;
-var COL_HEIGHT = 0;
-
-var START_X = 0;
-var START_Y = 0;
-
-var letter = undefined;
 
 var time = undefined;
 var current = undefined;
 
-var USED_LETTER = alphabet.scale(0.3,alphabet.A);
+var ALPHABET_LETTERS = alphabet.ALL;
 var saved = false;
+var letters = [];
+var drawnLetters = [];
+var TOTAL = ALPHABET_LETTERS.length
+var totalDone = 0;
 
 exports.setup = function(_ctx, _env){
   ctx = _ctx;
   env = _env;
 
-  COL_WIDTH = 15
-  COL_HEIGHT =15;
-
-  NUM_COLS = Math.ceil(ctx.canvas.width / COL_WIDTH)
-  NUM_ROWS = Math.ceil(ctx.canvas.height / COL_HEIGHT);
-
   time = Date.now()
 
-  START_X = ctx.canvas.width / 2 - 20
-  START_Y = ctx.canvas.height / 2 - 100
-  letter = new Letter(START_X, START_Y, USED_LETTER, ctx);
+  background("rgba(255,255,255,1)");
+  
+  var x = ctx.canvas.width / 2 - 20;
+  var y = ctx.canvas.height - 80;
+  // var index = Math.floor(Math.random() * TOTAL);
+  var index = 25;
+  var letter = new Letter(x, y, alphabet.scale(0.8,ALPHABET_LETTERS[index]), ctx);
+  letters.push(letter);
+  letter.draw()
+  env.save()
+
+  drawnLetters = letters.slice();
 }
 
 exports.draw = function() {
-  current = Date.now()
-  if(current > time) {
-    time = current
-    if(letter.evolve()) {
-      background("rgba(255,255,255,0.1)");
-      letter.draw()
-    } else {
-      if (!saved) {
-        // env.done()
-        saved = true;
-      }
-    }
-    // drawLetter()
-  }
-}
-
-function drawLetter() {
-  var total = USED_LETTER.length;
-  ctx.beginPath();
-  for(var i = 0; i < total; i+= 2) {
-    ctx.lineTo(START_X + parseFloat(USED_LETTER[i]), START_Y + parseFloat(USED_LETTER[i + 1]));
-  }
-  ctx.stroke();
+  
 }
 
 function background(color){
